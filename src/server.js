4c4b31df3a1393f5726f743858824966b50c6a22
Guilherme Santos <guilherme.santos@selfitacademias.com.br>
2026-09-15 17:38:59 -0300

    feat: autenticação de usuários

--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const autenticador = require('./routes/autenticador/login');
+
+app.use(express.json());
 const port = process.env.PORT;
+
+app.use(autenticador);
 
 app.listen(port, () => {
     console.log(`Aplicação rodando na porta ${port}.`);
